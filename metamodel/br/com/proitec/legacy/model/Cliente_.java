15449905c27e7bba3fdf5fa1703a362c83a0411e
fredyefra <fredweb3@gmail.com>
2022-07-23 20:15:23 -0300

    FEAT:Specifications inclusão.

---
+++ metamodel/br/com/proitec/legacy/model/Cliente_.java
@@ -0,0 +1,20 @@
+package br.com.proitec.legacy.model;
+
+import java.time.LocalDate;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Cliente.class)
+public abstract class Cliente_ {
+
+	public static volatile SingularAttribute<Cliente, String> telefone;
+	public static volatile SingularAttribute<Cliente, String> endereco;
+	public static volatile SingularAttribute<Cliente, LocalDate> dtCadastro;
+	public static volatile SingularAttribute<Cliente, String> nome;
+	public static volatile SingularAttribute<Cliente, String> email;
+	public static volatile SingularAttribute<Cliente, Long> identificador;
+
+}
+
