5fb80adff2036d4db716cf2a6af66037433afc32
fredyefra <fredweb3@gmail.com>
2022-07-23 20:04:27 -0300

    FEAT:SpecificationExecutor inclusão.

--- src/main/java/br/com/proitec/legacy/repository/ClienteRepository.java
+++ src/main/java/br/com/proitec/legacy/repository/ClienteRepository.java
@@ -1,11 +1,12 @@
 package br.com.proitec.legacy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import br.com.proitec.legacy.model.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> , JpaSpecificationExecutor<Cliente> {
 
 }
