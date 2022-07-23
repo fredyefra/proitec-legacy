3d6b1ece8098c5b3d120b772a0c68a618e74b5ce
Marcelo Lemes da Silveira <lemmarcelo@gmail.com>
2022-07-23 13:27:33 -0300

    REFACTOR: Renomeação da classe em função do tipo de teste

--- src/test/java/br/com/proitec/legacy/ClientResourceIntegrationTest.java
+++ src/test/java/br/com/proitec/legacy/ClientResourceIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static io.restassured.RestAssured.given;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class ClientResourceTest {
+public class ClientResourceIntegrationTest {
 
 Cliente clienteStub= new Cliente(
         10L,
