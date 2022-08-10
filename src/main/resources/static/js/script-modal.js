d9704fe5a657c5cb843341ef8e04e22e05f5362b
fredyefra <fredweb3@gmail.com>
2022-08-10 00:44:53 -0300

    FEAT: script modal para edição

--- src/main/resources/static/js/script-modal.js
+++ src/main/resources/static/js/script-modal.js
@@ -2,8 +2,9 @@
  * Script para renderizar o modal de confirmação ou não de exclusão
  */
 	
-	/*<script>*/
-	$(document).ready(function(){
+/*<script type="text/javascript">*/
+$(document).ready(function(){
+	
 	$(".eBtn").click(function(event){
 	
 	event.preventDefault();
@@ -13,4 +14,28 @@
 	$("#modalQuestion").modal('show');
 	  })
     });
-	/*</script>*/
+/*</script>*/
+
+
+/**
+ * Script para renderizar o modal de edição
+ */	
+
+/*<script type="text/javascript">*/
+$(document).ready(function(){
+	
+	$(".eBtn1").click(function(event){
+	
+	event.preventDefault();
+	var href = $(this).attr('href');
+    
+    $.get(href,function(cliente){
+      $("#id").val(cliente.identificador);
+      $("#nome").val(cliente.nome);
+      $("#telefone").val(cliente.telefone);
+    });
+    
+	$("#modalEdit").modal('show');
+	  })
+    });
+/*</script>*/
