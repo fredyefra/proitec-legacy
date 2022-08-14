938d37c684b6ccdb728254009482420612c92eaa
fredyefra <fredweb3@gmail.com>
2022-08-14 00:42:00 -0300

    REFACTOR: Alteração de construtor

--- src/main/java/br/com/proitec/legacy/configuration/DataService.java
+++ src/main/java/br/com/proitec/legacy/configuration/DataService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.proitec.legacy.enderecows.EnderecoWS;
 import br.com.proitec.legacy.model.Cliente;
+import br.com.proitec.legacy.model.EnderecoWS;
 import br.com.proitec.legacy.service.ClienteService;
 
 @Service
@@ -17,14 +17,11 @@ public class DataService {
 
 	public void startDB() {
 
-		EnderecoWS enderecoWS = new EnderecoWS(null, "12371720", "12371720", "12371720", "12371720", "12371720", "DF"); 
-		
+		EnderecoWS enderecoWS = new EnderecoWS(null, "70073-901", "SBS Quadra 1 Bloco G Lote 32", "12371720", "Asa Sul"); 
 		
 		Cliente c1 = new Cliente(null, "Martin Fowler", "(98) 333-1233","fowler@webmail.com", enderecoWS);
-		//Cliente c2 = new Cliente(null, "Guido van Rossum", "7º Avennue - New York", "(44) 222-1111","rossum@webmail.com");
-		//Cliente c3 = new Cliente(null, "James Gosling", "Palo Alto - California", "(91) 777-1111","gosling@webmail.com");
-		
-		
+		//Cliente c2 = new Cliente(null, "Guido van Rossum", "(44) 222-1111", "vanrossum@webmail.com",enderecoWS);
+		//Cliente c3 = new Cliente(null, "James Gosling", "(89) 222-4476", "gosling@webmail.com",enderecoWS);
 		
 		service.saveAll(List.of(c1));
 	}
