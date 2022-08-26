626e652acb2736a91d94a7db2307e2936ec6794c
fredyefra <fredweb3@gmail.com>
2022-08-25 22:34:33 -0300

    FEAT: Objetos mock para simular inclusao no banco de dados

--- src/main/java/br/com/proitec/legacy/configuration/DataService.java
+++ src/main/java/br/com/proitec/legacy/configuration/DataService.java
@@ -9,6 +9,14 @@ import br.com.proitec.legacy.model.Cliente;
 import br.com.proitec.legacy.model.EnderecoWS;
 import br.com.proitec.legacy.service.ClienteService;
 
+
+/**
+* Realiza o inserte inicial na base de dados 
+*
+* @author proitec-legacy
+* @see DataService
+*/
+
 @Service
 public class DataService {
 	
@@ -18,11 +26,13 @@ public class DataService {
 	public void startDB() {
 
 		EnderecoWS enderecoWS = new EnderecoWS(null, "70073-901", "SBS Quadra 1 Bloco G Lote 32", "12371720", "Asa Sul"); 
+		EnderecoWS enderecoWS1 = new EnderecoWS(null, "70073-901", "SBS Quadra 3 Bloco B", "12371720", "Asa Sul");
+		EnderecoWS enderecoWS2 = new EnderecoWS(null, "70073-901", "SAUN Quadra 5 Lote B Torres I", "12371720", "Asa Sul");
 		
 		Cliente c1 = new Cliente(null, "Martin Fowler", "(98) 333-1233","fowler@webmail.com", enderecoWS);
-		//Cliente c2 = new Cliente(null, "Guido van Rossum", "(44) 222-1111", "vanrossum@webmail.com",enderecoWS);
-		//Cliente c3 = new Cliente(null, "James Gosling", "(89) 222-4476", "gosling@webmail.com",enderecoWS);
+		Cliente c2 = new Cliente(null, "Guido van Rossum", "(44) 222-1111", "vanrossum@webmail.com",enderecoWS1);
+		Cliente c3 = new Cliente(null, "James Gosling", "(89) 555-4478", "gosling@webmail.com",enderecoWS2);
 		
-		service.saveAll(List.of(c1));
+		service.saveAll(List.of(c1,c2,c3));
 	}
 }
