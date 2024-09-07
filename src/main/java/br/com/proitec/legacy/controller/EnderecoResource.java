c00559c2a58eb1c29b715d5674e0ef301ea3da5c
fredyefra <fredweb3@gmail.com>
2024-09-07 19:53:51 -0300

    Feat: Refatoração ws cep.

--- src/main/java/br/com/proitec/legacy/controller/EnderecoResource.java
+++ src/main/java/br/com/proitec/legacy/controller/EnderecoResource.java
@@ -1,15 +1,14 @@
 package br.com.proitec.legacy.controller;
 
-import javax.ws.rs.core.MediaType;
-
+import br.com.proitec.legacy.enderecows.EnderecoConsumer;
+import br.com.proitec.legacy.model.EnderecoWS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.proitec.legacy.enderecows.EnderecoConsumer;
-import br.com.proitec.legacy.model.EnderecoWS;
+import javax.ws.rs.core.MediaType;
 
 /**
 * Disponibiliza o recurso de CEP da classe {@link EnderecoConsumer} para a classe {@link EnderecoWS}
@@ -27,7 +26,7 @@ public class EnderecoResource {
 
 	    @GetMapping(value = "/{cep}/ws", produces = MediaType.APPLICATION_JSON)
 		public EnderecoWS findCep(@PathVariable String cep) {
-	    	EnderecoWS enderecoWS = enderecoConsumer.enderecoConsumer(cep);
+	    	EnderecoWS enderecoWS = enderecoConsumer.enderecoConsumerv2(cep);
 	        return enderecoWS;
 	    }
 	}	
--- src/main/java/br/com/proitec/legacy/enderecows/EnderecoConsumer.java
+++ src/main/java/br/com/proitec/legacy/enderecows/EnderecoConsumer.java
@@ -11,6 +11,8 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.UniformInterfaceException;
 
 import br.com.proitec.legacy.model.EnderecoWS;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 /**
 * Classe responsavel por desserializar o Json especificado em um objeto do tipo {@link EnderecoWS}
@@ -19,13 +21,15 @@ import br.com.proitec.legacy.model.EnderecoWS;
 * @see EnderecoConsumer
 */
 
+
 @Service
 public class EnderecoConsumer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private EnderecoWS endereco;
-	
+
+	@Deprecated
 	public EnderecoWS enderecoConsumer(String cep) {
 		
 		try {
@@ -36,12 +40,27 @@ public class EnderecoConsumer implements Serializable {
 			Gson gson = new Gson();
 			Type type = new TypeToken<EnderecoWS>(){}.getType();
 			endereco = gson.fromJson(json, type);
-			
+
 		} catch (UniformInterfaceException e) {
 			System.out.println("CEP não localizado!" + e);
 		    endereco = new EnderecoWS();
 		}
 		
 		return endereco;
+	}
+
+	public EnderecoWS enderecoConsumerv2 (final String cep){
+
+		WebClient webClient = WebClient.create("https://viacep.com.br/viacep.com.br");
+
+		return  webClient
+				.get()
+				.uri(uriBuilder -> uriBuilder.path("/ws/")
+						.path(cep)
+						.path("/json/")
+						.build()).retrieve()
+				.bodyToMono(EnderecoWS.class)
+				.block();
+
 	}
 }
